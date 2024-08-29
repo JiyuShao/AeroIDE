cb0541e2f642865a0046ebe70c1bc68fb5d0c000
Jiyu Shao <jiyu.shao@gmail.com>
2024-08-29 17:19:39 +0800

    fix(webview): fix load webview logic

--- packages/extensions/js-runner-and-debugger/src/utils/webview/webview/content-provider.ts
+++ packages/extensions/js-runner-and-debugger/src/utils/webview/webview/content-provider.ts
@@ -1,46 +1,82 @@
 import * as vscode from 'vscode';
-import { isProduction } from '../../../config';
+// import { isProduction } from '../../../config';
 
 export default class ContentProvider {
   async getDevServerContent(
     _context: vscode.ExtensionContext,
-    webviewView: vscode.WebviewView
+    _webviewView: vscode.WebviewView
   ) {
     return `<!DOCTYPE html>
     <html lang="en">
     <head>
-        <script type="module" src="http://localhost:3000/@vite/client"></script>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body data-view="${webviewView.viewType}">
-      <div id="app"></div>
+      <meta charset="UTF-8">
+      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>NPM Webview</title>
+      <script type="module" src="http://localhost:3000/@vite/client"></script>
       <script type="module" src="http://localhost:3000/src/main.ts"></script>
+    </head>
+    <body>
+      <div id="app">NPM Webview</div>
     </body>
     </html>
-      `;
+    `;
   }
 
   async getProductionContent(
     context: vscode.ExtensionContext,
-    _webviewView: vscode.WebviewView
+    webviewView: vscode.WebviewView
   ) {
-    const webviewHtmlUri = vscode.Uri.joinPath(
-      context.extensionUri,
-      'assets/webview/index.html'
-    );
-    const webviewHtmlContent =
-      await vscode.workspace.fs.readFile(webviewHtmlUri);
-    return new TextDecoder().decode(webviewHtmlContent);
+    try {
+      const manifestJsonUri = vscode.Uri.joinPath(
+        context.extensionUri,
+        'assets/webview/.vite/manifest.json'
+      );
+      const manifestJson = JSON.parse(
+        new TextDecoder().decode(
+          await vscode.workspace.fs.readFile(manifestJsonUri)
+        )
+      );
+      const scriptUri = webviewView.webview.asWebviewUri(
+        vscode.Uri.joinPath(
+          context.extensionUri,
+          `assets/webview/${manifestJson['index.html'].file}`
+        )
+      );
+      const styleUri = webviewView.webview.asWebviewUri(
+        vscode.Uri.joinPath(
+          context.extensionUri,
+          `assets/webview/${manifestJson['index.html'].css[0]}`
+        )
+      );
+      return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>NPM Webview</title>
+        <script type="module" src="${scriptUri}"></script>
+        <link rel="stylesheet" crossorigin href="${styleUri}">
+      </head>
+      <body>
+        <div id="app">NPM Webview</div>
+      </body>
+      </html>
+      `;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   async getContent(
     context: vscode.ExtensionContext,
     webviewView: vscode.WebviewView
   ) {
-    if (isProduction) {
-      return this.getProductionContent(context, webviewView);
-    }
-    return this.getDevServerContent(context, webviewView);
+    // if (!isProduction) {
+    //   return this.getDevServerContent(context, webviewView);
+    // }
+    return this.getProductionContent(context, webviewView);
   }
 }
